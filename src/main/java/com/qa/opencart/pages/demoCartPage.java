a71e1a384d5f1dd383660a5321d7d67176c932e8
Venkatesh M <venkatesh.m@metricstream.com>
2022-05-20 22:40:57 +0530

    added file in cartpage branch

---
+++ src/main/java/com/qa/opencart/pages/demoCartPage.java
@@ -0,0 +1,13 @@
+package com.qa.opencart.pages;
+
+public class demoCartPage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int i = 10;
+		System.out.println(i);
+
+	}
+
+}
