2c5143302d87ee1e549186f8bbe2ce3475d8f4a6
Venkatesh M <venkatesh.m@metricstream.com>
2022-05-20 23:55:05 +0530

    Updated cart page PR request

--- src/main/java/com/qa/opencart/pages/demoCartPage.java
+++ src/main/java/com/qa/opencart/pages/demoCartPage.java
@@ -1,13 +1,19 @@
 package com.qa.opencart.pages;
 
+import org.openqa.selenium.By;
+
 public class demoCartPage {
+	
+	private By cart = By.id("cart");
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
 		
 		int i = 10;
+		
+		
 		System.out.println(i);
-
+		System.out.println("done");
 	}
 
 }
